1a5722cf5bdf40f4b659f0b5e9307a91ad680a26
Henry J. Garcia <hjgarcia1@gmail.com>
2021-01-18 06:50:41 -0500

    initial commit

---
+++ main.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+// Max number of candidates
+#define MAX 9
+
+// Candidates have name and vote count
+typedef struct
+{
+    char *name;
+    int votes;
+} candidate;
+
+// Array of candidates
+candidate candidates[MAX];
+
+// Number of candidates
+int candidate_count;
+
+// Function prototypes
+bool vote(char *name);
+void print_winner(void);
+
+int main(int argc, char *argv[])
+{
+    // Check for invalid usage
+    if (argc < 2)
+    {
+        printf("Usage: plurality [candidate ...]\n");
+        return 1;
+    }
+
+    // Populate array of candidates
+    candidate_count = argc - 1;
+    if (candidate_count > MAX)
+    {
+        printf("Maximum number of candidates is %i\n", MAX);
+        return 2;
+    }
+    for (int i = 0; i < candidate_count; i++)
+    {
+        candidates[i].name = argv[i + 1];
+        candidates[i].votes = 0;
+    }
+
+    int voter_count;
+    printf("Number of voters: ");
+    scanf("%d", &voter_count);
+
+    // Loop over all voters
+    for (int i = 0; i < voter_count; i++)
+    {
+        char name[64];
+        printf("Vote: ");
+        scanf("%s", name);
+
+        // Check for invalid vote
+        if (!vote(name))
+        {
+            printf("Invalid vote.\n");
+        }
+    }
+
+    // Display winner of election
+    print_winner();
+}
+
+// Update vote totals given a new vote
+bool vote(char *name)
+{
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(candidates[i].name, name) == 0)
+        {
+            candidates[i].votes++;
+            return true;
+        }
+    }
+
+    return false;
+}
+
+// Print the winner (or winners) of the election
+void print_winner(void)
+{
+    //sort the array by votes desc grab the first
+
+    //from the first one grab the vote. 
+
+    //then grab the highest winners. print the results
+
+    printf("We have a winner");
+    return;
+}
